c99563eb447d21fff239f673040367781eff8da7
Yannick <yannick@knapgemaakt.nl>
2026-02-04 22:08:42 +0100

    fix: email colors displaying incorrectly on iPhone (#23) 🐛
    Closes #22

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
+    trailingSlash: 'never',
     adapter: cloudflare(),
     vite: {
         plugins: [tailwind()],
